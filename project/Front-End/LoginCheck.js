a141928b9d0be5d7a36aa1c1ce06261a3b8db128
Jinming Zhang <zhan2754@IITS-I406-20.utsc-labs.utoronto.ca>
2017-11-01 11:24:18 -0400

    fix little bug on login page

--- project/Front-End/LoginCheck.js
+++ project/Front-End/LoginCheck.js
@@ -1,18 +1,20 @@
 function checkLogin(form){
 	if(form.name =="InsTALoginForm"){
 		// check condition for Ins/TA to login
-		if(form.userName.value == "ta"){
+		if(form.userName.value === "ta"){
 			form.action = "welcome.html";
 		}else{
 			alert("GIVE ME 4.0!!");
+			form.action = "./InsTaLgin.html";
 		}
 	}
 	else{
 		// check condition for student to login
-		if(form.userName.value =="student"){
+		if(form.userName.value ==="student"){
 			form.action = "welcome.html";
 		}else{
 			alert("YOU SHALL NOT PASS@_@");
+			form.action = "./studentLgin.html";
 		}
 	}
 }
